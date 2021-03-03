7b67dc238d8ae606de8a575b73ed04a7b6058ed5
migueleyen <migueleyen@gmail.com>
2021-03-02 19:39:10 -0500

    add console time

--- _udemy.js
+++ _udemy.js
@@ -1,10 +1,12 @@
 const { cookAndDeliverFood } = require("./_cookOrder");
 
 function placeanOrder(order) {
+  console.time("inicio");
   console.log("Customer Order : ", order);
 
   cookAndDeliverFood(order, function (customer) {
     console.log("Deliver Food for : ", customer);
+    console.timeEnd("inicio");
   });
 }
 
